1dbb3cf08b0dc6ef7971512c7290b65b5ed05af1
Jumail Mundekkat <mundekkat@hotmail.com>
2023-05-12 16:44:11 +1000

    Add time libraries (and helpful decl)

--- src/support/store.ts
+++ src/support/store.ts
@@ -1,15 +1,16 @@
 import { Dependency, Logger } from 'bot-framework';
 import IORedis, { Redis } from 'ioredis';
 
+const REDIS_PREFIX = "timebot_";
+
 /*
   API class to interact with underlying storage implementation
   In this case, Redis
 
   Schema:
-  TODO: Write up a schema. Pls namespace is with timebot_* cause this is a shared Redis
+    timebot_<guildid>_<userid> = timezone string set for user
 */
 class StoreImpl {
-
   // Redis client
   rclient: Redis;
   // General logger
@@ -32,7 +33,14 @@ class StoreImpl {
       StoreDependency.ready();
     });
   }
- 
+  
+  public async getTimeZone(guildId: string, userId: string): Promise<string> {
+    return await this.rclient.get(REDIS_PREFIX + `${guildId}_${userId}`);
+  }
+
+  public async setTimeZone(guildId: string, userId: string, timezone: string): Promise<void> {
+    await this.rclient.set(REDIS_PREFIX + `${guildId}_${userId}`, timezone);
+  }
 }
 
 export const Store = new StoreImpl();
